80331f1667290a57545d53ef7ac878b6754a8c3f
TYC33 <cty900728@gmail.com>
2023-06-03 22:30:29 +0800

    second commit

--- ppt_gen2.py
+++ ppt_gen2.py
@@ -28,7 +28,7 @@ if __name__ == '__main__':
     col6, col7 = st.columns(2)
     
     title = col6.text_input("請輸入標題:", value = '要如何學習機器學習')
-    sub_title = col7.text_input("請輸入副標題:", value = '李御璽/銘傳大學資工系')
+    sub_title = col7.text_input("請輸入副標題:", value = '陳姿羽/銘傳大學資工系')
     
     col8, col9 = st.columns([1,4])
     
@@ -54,15 +54,21 @@ if __name__ == '__main__':
             item_list = []
             
             for line in context.splitlines():
+                
                 item_list.append(line.strip())
+                    
+                
             
             chunked_lists = split_list(item_list, num_of_points)
+            
+            #print(chunked_lists)
 
             slides = []
 
             # Print the chunked lists
             for sublist in chunked_lists:
                 slides.append({'title': context_title, 'content': sublist})
+                
             
             create_body(prs, slides)
     
